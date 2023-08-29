09da21f36a01cc49d07ec6b1909f635641b90143
Your Name <you@example.com>
2023-08-29 20:03:39 +0300

    added_if/else

--- classwork_loops.js
+++ classwork_loops.js
@@ -1,3 +1,17 @@
 let num215 = 215
 
-if (num215 == 215) {console.log('yes');}
+if (num215 == 215) {
+    console.log('yes');
+    console.log('1');
+    console.log('2');
+}
+
+if (num215>= 215 && num215 <250) {
+    console.log('Works fine');
+}
+
+if (num215 > 300) {
+    console.log('num215 not work');
+} else {
+    console.log('work');
+}
