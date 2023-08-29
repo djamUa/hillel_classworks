1a8c5b3f8eef5e10ebca3b94681c3157d8218c67
Your Name <you@example.com>
2023-08-29 20:24:51 +0300

    add_multiple_if_else

--- classwork_loops.js
+++ classwork_loops.js
@@ -15,3 +15,19 @@ if (num215 > 300) {
 } else {
     console.log('work');
 }
+
+
+if (num215 > 300) {
+    console.log('num215 not work');
+} else if (num215>= 250) {
+    console.log('greate 250');
+}
+else if  (num215>= 200) {
+    console.log('greate 200');
+}
+else if (Number.isInteger(num215)) {
+    console.log('num is int');
+}
+else { 
+    console.log('else');
+}
